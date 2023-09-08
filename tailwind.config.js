d81eebd0080558225c33c521ba62a316a88ce1a6
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2023-09-08 16:34:20 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        roboto:"roboto",
+        inter:"inter",
+      }
+    },
+  },
+  plugins: [],
+}
+
